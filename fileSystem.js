207461c6910595594867d2686e93570f6d6f4fbb
raytchellee <amakaoforkaja@gmail.com>
2022-10-29 18:58:40 +0100

    created new file and updated html and css files

--- fileSystem.js
+++ fileSystem.js
@@ -1,19 +1,67 @@
-//file system require node js
-const fs = require('fs');
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-//read files
 const server = http.createServer((req,res) => {
-    fs.readFile('index.html', (err,data) => {
-        if(err) throw err;
-        // console.log(data);
-        res.writeHead(200,{
-            'Content-Type': 'text/html'
-        });
-        res.write(data);
-        res.end();
+    let filePath = path.join(__dirname,'public', req.url == '/' ? 'index.html' : req.url)
+    console.log(filePath);
+    let contentType = getContentType(filePath) || 'index.html'
+    let emptyPath = path.join(__dirname,'public','404.html')
+    // fs.readFile(filePath,'utf8', (err,content) => {
+    fs.readFile(filePath, (err,content) => {
+        if(err){    
+            if(err.code == 'ENOENT'){
+                // console.log('errrrr===',err)
+                fs.readFile(emptyPath,'utf8',(err,content) => {
+                    res.writeHead(200, {'Content-Type': contentType});
+                    res.end(content);
+                })
+            }else{
+                res.writeHead(500);
+                res.end('<h1>A server error has occured</h1>')
+            }
+        }
+        if(!err){
+            res.writeHead(200,{'Content-Type': contentType});
+            res.end(content);
+        }
     })
+    console.log(req.url)
 })
 
-server.listen(7500, '127.0.0.1');
-console.log('it really worked!');
+const getContentType = (filePath) => {
+    // console.log(filePath)
+    let extname = path.extname(filePath);
+    if(extname == '.js'){
+        return 'text/javascript'
+    }
+    if(extname == '.css'){
+        return 'text/css'
+    }
+    if(extname == '.js'){
+        return 'text/html'
+    }
+    if(extname == '.png'){
+        // console.log('pnggggggg');
+        return 'image/png'
+        
+    }
+    if(extname == '.jpg'){
+        // console.log('jpggggggg');
+        return 'image/jpg'
+    }
+    if(extname == '.PNG'){
+        // console.log('Pnggggggg');
+        return 'image/png'
+    }
+    if(extname == '.JPG'){
+        // console.log('Jpggggggg');
+        return 'image/jpg'
+    }
+}
+
+const port = 7500;
+
+server.listen(port,'127.0.0.1',() => {
+    console.log(`Server is running on ${port}`)
+})
